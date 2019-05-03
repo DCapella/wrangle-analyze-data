7d7d1bb7e496484fbd27fe81f01ca592a4a77465
David Capella <D_Capella@yahoo.com>
2019-05-03 07:50:15 -0500

    finished Finished the wrangle project for Udacity. The turn in for the project is in the folder labled, UDACITY

---
+++ twitter_keys.py
@@ -0,0 +1,4 @@
+consumer_key = 'TWzHjkO9FcKil1uxHds3kZMen'
+consumer_secret = '3NQYeR2MDbcacKqif0zP8YpamN8Q9uhvTT5Y4FfNhPjTCmNS3V'
+access_token = '1117765671952310272-zqQweNlWrnItaDPtw20Ln3cRnBWgQr'
+access_secret = 'uBQx8ZbTpsV4B31w3UZgDta5rqjmN2URh61F1DyqaLxuT'
